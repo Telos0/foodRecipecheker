eeb2a91e000769265d545b316d67f884395cf762
telos <my4357909@naver.com>
2021-12-22 13:20:30 +0000

    ADD recipe edit function

--- foodcheck/urls.py
+++ foodcheck/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name = 'foodcheck'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<str:food_name>/', views.foodingredients, name='foodingredients'),
+    path('foodingeredientslist/<str:food_name>/', views.foodingredients, name='foodingredients'),
+    path('regfood/', views.regfood, name='regfood'),
+    path('regfood/get_food/', views.get_food, name='regfood_new'),
+    path('regfood/get_foodingredients/<str:new_food_name>/', views.get_foodingredients, name='regfoodingredients_new'),
+    path('regfood/add_foodingredients/<str:food_name>/', views.add_foodingredients, name='add_regfoodingredients'),
+    path('foodingeredientslist/del_foodingeredients/<str:food_name>/', views.del_foodingeredients, name='del_foodingeredients'),
+
 ]
--- foodcheck/views.py
+++ foodcheck/views.py
@@ -2,13 +2,154 @@ from django.shortcuts import render, get_object_or_404
 from foodcheck.models import Food, FoodIngredients, Ingredients
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
+from django.utils import timezone
 
+#form class 선언
+class FoodForm(forms.Form):
+    food_name = forms.CharField(max_length=200)
+
+class FoodIngredientsForm(forms.Form):
+    ingredients_name = forms.CharField(max_length=200)
+
+
+#함수
 def home(request):
-    reg_food_list = Food.objects.all().order_by('-insdttm')[:5]
+    reg_food_list = Food.objects.all().order_by('-insdttm')
     context = {'reg_food_list': reg_food_list}
     return render(request, 'foodcheck/home.html', context)
 
 def foodingredients(request, food_name):
     foodingredients_list = FoodIngredients.objects.filter(food = food_name)
+    context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+    return render(request, 'foodcheck/foodingredients.html', context)
+
+def regfood(request):
+    return render(request, 'foodcheck/regfood.html')
+
+def get_food(request):
+    if request.method == 'POST':
+        form = FoodForm(request.POST)
+
+
+        if form.is_valid():
+            new_food_name = form.cleaned_data['food_name']
+            new_insdttm = timezone.now()
+
+            old_food = Food.objects.filter(food_name = new_food_name)
+            for food in old_food:
+                if food.food_name is not None:
+                    return render(request, 'foodcheck/regfood.html', {'error_message': "That is already exist"})
+            #new food 정의
+            new_Food = Food(food_name = new_food_name, insdttm = new_insdttm)
+            new_Food.save()
+
+            #redirect
+            return render(request, 'foodcheck/regfoodingredients.html', {'new_food_name': new_food_name})
+        else:
+            reg_food_list = Food.objects.all().order_by('-insdttm')
+            context = {'reg_food_list': reg_food_list}
+            return render(request, 'foodcheck/home.html', context)
+    else:
+        reg_food_list = Food.objects.all().order_by('-insdttm')
+        context = {'reg_food_list': reg_food_list}
+    return render(request, 'foodcheck/home.html', context)
+
+#음식레시피 초기등록화면에서 호출하여 레시피를 추가
+def get_foodingredients(request, new_food_name):
+    if request.method == 'POST':
+        form = FoodIngredientsForm(request.POST)
+        if form.is_valid():
+            new_ingredients_name = form.cleaned_data['ingredients_name']
+            new_insdttm = timezone.now()
+
+            new_food_object = Food.objects.get(food_name = new_food_name)
+            new_ingredients_object = Ingredients.objects.get(ingredients_name = new_ingredients_name)
+
+
+            old_ingredients = FoodIngredients.objects.filter(food = new_food_object, ingredients = new_ingredients_object)
+            for ingrediendts in old_ingredients:
+                if ingrediendts.food is not None:
+                    return render(request, 'foodcheck/regfoodingredients.html', {'new_food_name' : new_food_name, 'error_message': "That is already exist"})
+            #new foodingredients 정의
+            new_Foodingredients = FoodIngredients(food = new_food_object, ingredients = new_ingredients_object, insdttm = new_insdttm)
+            new_Foodingredients.save()
+
+            foodingredients_list = FoodIngredients.objects.filter(food = new_food_name)
+            #redirect
+            return render(request, 'foodcheck/regfoodingredients.html', {'new_food_name': new_food_name, 'foodingredients_list': foodingredients_list})
+        else:
+            render(request, 'foodcheck/regfoodingredients.html', {'new_food_name': new_food_name})
+    else:
+        foodingredients_list = FoodIngredients.objects.filter(food=new_food_name)
+
+    return render(request, 'foodcheck/regfoodingredients.html', {'new_food_name': new_food_name, 'foodingredients_list': foodingredients_list})
+
+#음식레시피 수정화면에서 호출하여 레시피를 삭제
+def del_foodingeredients(request, food_name):
+    if request.method == 'POST':
+        print(request.POST)
+        form = FoodIngredientsForm(request.POST)
+        if form.is_valid():
+            ingredients_name = form.cleaned_data['ingredients_name']
+
+            food_object = Food.objects.get(food_name = food_name)
+            ingredients_object = Ingredients.objects.get(ingredients_name = ingredients_name)
+
+
+            FoodIngredients.objects.filter(food = food_object, ingredients = ingredients_object).delete()
+
+            #redirect
+            foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+            context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+            return render(request, 'foodcheck/foodingredients.html', context)
+        else:
+            foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+            context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+            render(request, 'foodcheck/foodingredients.html', context)
+    else:
+        foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+        context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+        return render(request, 'foodcheck/foodingredients.html', context)
+    foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+    context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+    return render(request, 'foodcheck/foodingredients.html', context)
+
+#음식레시피 수정화면에서 호출하여 레시피를 추가
+def add_foodingredients(request, food_name):
+    if request.method == 'POST':
+        form = FoodIngredientsForm(request.POST)
+        if form.is_valid():
+            ingredients_name = form.cleaned_data['ingredients_name']
+            insdttm = timezone.now()
+
+            food_object = Food.objects.get(food_name = food_name)
+            ingredients_object = Ingredients.objects.get(ingredients_name = ingredients_name)
+
+
+            old_ingredients = FoodIngredients.objects.filter(food = food_object, ingredients = ingredients_object)
+            for ingrediendts in old_ingredients:
+                if ingrediendts.food is not None:
+                    foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+                    context = {'foodingredients_list': foodingredients_list, 'food_name': food_name, 'error_message': "That is already exist"}
+                    return render(request, 'foodcheck/foodingredients.html', context)
+            #new foodingredients 정의
+            new_Foodingredients = FoodIngredients(food = food_object, ingredients = ingredients_object, insdttm = insdttm)
+            new_Foodingredients.save()
+
+            #redirect
+            foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+            context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+            return render(request, 'foodcheck/foodingredients.html', context)
+        else:
+            foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+            context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+            return render(request, 'foodcheck/foodingredients.html', context)
+    else:
+        foodingredients_list = FoodIngredients.objects.filter(food=food_name)
+        context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
+        return render(request, 'foodcheck/foodingredients.html', context)
+
+    foodingredients_list = FoodIngredients.objects.filter(food=food_name)
     context = {'foodingredients_list': foodingredients_list, 'food_name': food_name}
     return render(request, 'foodcheck/foodingredients.html', context)
